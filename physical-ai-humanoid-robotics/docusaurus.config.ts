e744781d3e320d32764bc9ebd810a1fa28a5c342
shahzad1050 <shahzadarham266@gmail.com>
2025-12-09 23:20:58 +0500

    Update GitHub Pages configuration

--- physical-ai-humanoid-robotics/docusaurus.config.ts
+++ physical-ai-humanoid-robotics/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://user.github.io',
+  url: 'https://shahzad1050.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/physical-ai-humanoid-robotics/',
+  baseUrl: '/Physical-AI-Humanoid-book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'user', // Usually your GitHub org/user name.
-  projectName: 'physical-ai-humanoid-robotics', // Usually your repo name.
+  organizationName: 'shahzad1050', // Usually your GitHub org/user name.
+  projectName: 'Physical-AI-Humanoid-book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
